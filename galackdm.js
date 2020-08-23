37d9e58ea935d5fc2ce4f3c921ee19411648baad
GalackQSM <44106790+GalackQSM@users.noreply.github.com>
2020-08-23 14:55:25 +0200

    Update galackdm.js

--- galackdm.js
+++ galackdm.js
@@ -52,7 +52,8 @@ nitro.on('ready', () => {
   console.log("");                                   
   console.log((chalk.red(`                                                       Discord: https://discord.gg/XH7zQ8s`)));   
   console.log((chalk.red(`                                                     Twitter: https://twitter.com/Galack_QSM`)));   
-  console.log((chalk.red(`                                                       Github: https://github.com/GalackQSM`)));   
+  console.log((chalk.red(`                                                       Github: https://github.com/GalackQSM`))); 
+  console.log((chalk.red(`                                                       Youtube: https://youtube.com/GalackQSM`)));   
   console.log("");                                  
 
    console.log(`Statistiques globales : \n\nLe bot a un total de ${nitro.guilds.cache.size} serveurs. \nPour un total de ${nitro.users.cache.size} membres. \n\nMessage :\n\u001b[36m${messagetsend}\u001b[0m\n\n\n `)
